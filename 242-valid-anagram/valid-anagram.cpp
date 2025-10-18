be55915ccdece8b6edf4cf48185b1c00217ac98e
Asmi Srivastava <srivasmi20@gmail.com>
2025-10-19 01:16:24 +0530

    Time: 1 ms (60.29%) | Memory: 9.7 MB (74.16%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t) return true;
-        else return false;
+         unordered_map<char,int> mpp;
+         for(auto it:s) {mpp[it]++;}
+         for(auto it:t){
+            mpp[it]--;
+         }
+         for(auto it:mpp){
+            if(it.second!=0) return false;
+         }
+         return true;
+        
     }
 };
