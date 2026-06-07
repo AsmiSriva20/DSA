c02c43748cbaafc5b96c1975a4f0d1246646067d
Asmi Srivastava <srivasmi20@gmail.com>
2026-06-08 01:53:08 +0530

    Time: 3 ms (18.68%) | Memory: 9 MB (23.94%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        int c=0;
+        int counter=0;
         string ans="";
         for(int i=0;i<s.size();i++){
-            if(s[i]==')') c--;
-            if(c!=0) ans.push_back(s[i]);
-            if(s[i]=='(')c++;
+            if(s[i]==')') counter--;
+            if(counter!=0) ans.push_back(s[i]);
+            if(s[i]=='(') counter++;
         }
-
-     return ans;
-        
+        return ans;
     }
 };
