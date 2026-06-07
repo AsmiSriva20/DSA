f1060bbf8de88f1d3668cee506b7f318a57a208d
Asmi Srivastava <srivasmi20@gmail.com>
2026-06-08 01:35:53 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (69.45%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -4,19 +4,20 @@ public:
         reverse(s.begin(),s.end());
         int n=s.length();
         int i=0,l=0,r=0;
+
         while(i<n){
             while(i<n && s[i]!=' '){
                 s[r]=s[i];
                 r++;i++;
             }
-            if(l<r){reverse(s.begin()+l,s.begin()+r);
-            s[r]=' ';
-            r++;
-            l=r;
+            if(l<r){
+                reverse(s.begin()+l,s.begin()+r);
+                s[r]=' ';
+                r++;
+                l=r;
             }
             i++;
         }
-
         return s.substr(0,r-1);
     }
 };
