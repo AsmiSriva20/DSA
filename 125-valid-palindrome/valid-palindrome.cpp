467f9e880dd249eae3b0ac515d101c6adc1c20b6
Asmi Srivastava <srivasmi20@gmail.com>
2026-06-08 13:26:56 +0530

    Time: 1 ms (43.55%) | Memory: 9.8 MB (93.96%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -5,11 +5,11 @@ public:
         while(l<r){
             while(l<r && !isalnum(s[l])) l++;
             while(l<r && !isalnum(s[r])) r--;
-
-            if(tolower(s[l])!=tolower(s[r])) return false;
-            l++;
-            r--;
-        }
-        return true;
+        
+        if(tolower(s[l])!=tolower(s[r])) return false;
+        l++;
+        r--;
+    }
+    return true;
     }
 };
