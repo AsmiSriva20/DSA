5e6ffb2b4ccf2935f08870f323ac628cf30c074e
Asmi Srivastava <srivasmi20@gmail.com>
2026-06-10 22:30:18 +0530

    Time: 0 ms (100.00%) | Memory: 22.2 MB (60.82%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalin(string &s, int l, int r) {
+        while (l < r) {
+            if (s[l] != s[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+
+        while (l < r) {
+            if (s[l] == s[r]) {
+                l++;
+                r--;
+            } else {
+                return isPalin(s, l + 1, r) ||
+                       isPalin(s, l, r - 1);
+            }
+        }
+
+        return true;
+    }
+};
