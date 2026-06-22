59f94c84929562eb0d84aa71ff0306fa299bc393
Asmi Srivastava <srivasmi20@gmail.com>
2026-06-22 12:16:33 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (72.20%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
-    int countPartitions(vector<int>& nums, int maxSum) {
-        int partitions = 1;
-        long long sum = 0;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (sum + nums[i] <= maxSum) {
-                sum += nums[i];
-            } else {
-                partitions++;
-                sum = nums[i];
+    int num_of_partitions(vector<int>&nums, int maxsum){
+        long long sum=0;
+        int parts=1;
+        for(int i=0;i<nums.size();i++){
+            if(sum+nums[i]<=maxsum){
+                sum+=nums[i];
+            }
+            else{
+                parts++;
+                sum=nums[i];
             }
         }
 
-        return partitions;
+    return parts;
     }
-
     int splitArray(vector<int>& nums, int k) {
-        int low = *max_element(nums.begin(), nums.end());
-        int high = accumulate(nums.begin(), nums.end(), 0);
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            int partitions = countPartitions(nums, mid);
-
-            if (partitions > k)
-                low = mid + 1;
-            else
-                high = mid - 1;
+        int low= *max_element(nums.begin(),nums.end());
+        int high = accumulate(nums.begin(),nums.end(),0);
+        while(low<=high){
+            int mid= low+(high-low)/2;
+            int partitions=num_of_partitions(nums,mid);
+           
+            if(partitions>k) low=mid+1;
+            else high=mid-1;
         }
 
-        return low;
+return low;
+        
     }
 };
