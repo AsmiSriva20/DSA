0afb707b6b6e7b58f5d189eba74e481db58bd476
Asmi Srivastava <srivasmi20@gmail.com>
2026-06-25 10:19:28 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (47.68%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -14,23 +14,23 @@ public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> ans;
         if(root==NULL) return ans;
-        bool LtoR = true;
-        queue<TreeNode*>q;
+        queue<TreeNode*> q;
         q.push(root);
+        bool LtoR= true;
         while(!q.empty()){
-             int size= q.size();
-             vector<int> level(size);
+            int size=q.size();
+            vector<int>level(size);
             for(int i=0;i<size;i++){
                 TreeNode* node= q.front();
                 q.pop();
-                int index= LtoR? i: size-i-1;
-                level[index] = node->val;
+                int index= LtoR? i: size-1-i;
+                level[index]= node->val;
                 if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-               
+                if(node->right)q.push(node->right);
             }
             ans.push_back(level);
-            LtoR= !LtoR;
+            LtoR=!LtoR;
+
         }
         return ans;
         
