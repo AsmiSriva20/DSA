80d6fd255bc370b087fa73d9a01d2f17c41a1eb9
Asmi Srivastava <srivasmi20@gmail.com>
2026-06-30 17:35:42 +0530

    Time: 0 ms (100.00%) | Memory: 61 MB (26.78%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -11,48 +11,29 @@
  */
 class Solution {
 public:
-    TreeNode* prev = NULL;
-    TreeNode* first = NULL;
-    TreeNode* middle = NULL;
-    TreeNode* last = NULL;
-
-    void in(TreeNode* root)
-    {
-        if (root == NULL)
-        {
-            return;
-        }
-
-        in(root->left);
-
-        if (prev != NULL && prev->val > root->val)
-        {
-            if (first == NULL)
-            {
-                first = prev;
+    TreeNode* first;
+    TreeNode* prev;
+    TreeNode* middle;
+    TreeNode* last;
+    void inorder(TreeNode* root){
+        if(root==NULL) return;
+        inorder(root->left);
+        if(prev!=NULL && (prev->val>root->val)){
+            if(first==NULL){
+                first=prev;
                 middle = root;
             }
-            else
-            {
-                last = root;
-            }
+            else last=root;
         }
-
-        prev = root;
-
-        in(root->right);
+        prev=root;
+        inorder(root->right);
     }
 
     void recoverTree(TreeNode* root) {
-        in(root);
-
-        if (last == NULL)
-        {
-            swap(first->val, middle->val);
-        }
-        else
-        {
-            swap(first->val, last->val);
-        }
+        first=middle=last=prev=NULL;
+        prev = new TreeNode(INT_MIN);
+        inorder(root);
+        if(first && last) swap(first->val,last->val);
+        else if(first && middle) swap(first->val,middle->val);
     }
 };
