501ee432ea3250d9b0e9e0901b8b81fbd4c442c0
Asmi Srivastava <srivasmi20@gmail.com>
2026-07-01 11:30:23 +0530

    Time: 0 ms (100.00%) | Memory: 104.3 MB (77.91%) - LeetSync

--- 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
@@ -1,51 +1,44 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    class Nodeval{
 
-    public: 
-        int sum; int Minval; int Maxval;
+    class nodevalue {
+    public:
+        int sum;
+        int minval;
+        int maxval;
 
-        Nodeval(int sum, int Minval, int Maxval){
-            this->sum=sum;
-            this->Minval=Minval;
-            this->Maxval=Maxval;
+        nodevalue(int sum, int minval, int maxval) {
+            this->sum = sum;
+            this->minval = minval;
+            this->maxval = maxval;
         }
-
     };
 
     int ans;
 
-    Nodeval postorder(TreeNode* root){
+    nodevalue postorder(TreeNode* root) {
 
-        if(root==NULL) return {0,INT_MAX, INT_MIN};
+        if(root == NULL)
+            return {0, INT_MAX, INT_MIN};
 
-        Nodeval left=postorder(root->left);
-        Nodeval right=postorder(root->right);
+        nodevalue left = postorder(root->left);
+        nodevalue right = postorder(root->right);
 
-        if(root->val<=left.Maxval || root->val>=right.Minval){
-            return {max(left.sum,right.sum),INT_MIN, INT_MAX};
-        }
-        ans=max(ans,left.sum+right.sum+root->val);
-        return {left.sum+right.sum+root->val,
-                min(root->val,left.Minval),
-                max(right.Maxval,root->val)};
+        if(root->val <= left.maxval || root->val >= right.minval)
+            return {max(left.sum, right.sum), INT_MIN, INT_MAX};
+
+        ans = max(ans, left.sum + right.sum + root->val);
+
+        return {
+            left.sum + right.sum + root->val,
+            min(root->val, left.minval),
+            max(root->val, right.maxval)
+        };
     }
 
     int maxSumBST(TreeNode* root) {
-        ans=0;
+        ans = 0;
         postorder(root);
         return ans;
-
     }
 };
