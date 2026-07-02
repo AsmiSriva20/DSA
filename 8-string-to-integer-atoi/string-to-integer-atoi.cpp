a8eeb5e80f3bcfcff6a8d027bc2519894dcbc5fd
Asmi Srivastava <srivasmi20@gmail.com>
2026-07-02 10:35:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (11.50%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    int i = 0, sign = 1;
-
-    long long solve(string &s, long long num) {
-        if (i == s.size() || !isdigit(s[i])) return num;
-        num = num * 10 + (s[i++] - '0');
-
-        if (sign == 1 && num > INT_MAX) return INT_MAX;
-        if (sign == -1 && -num < INT_MIN) return -(long long)INT_MIN;
-
-        return solve(s, num);
-    }
-
     int myAtoi(string s) {
+        int i = 0, sign = 1;
+        long long result = 0;
+
         while (i < s.size() && s[i] == ' ') i++;
+
         if (i < s.size() && (s[i] == '+' || s[i] == '-'))
             sign = s[i++] == '-' ? -1 : 1;
 
-        long long num = solve(s, 0);
-        return sign == 1 ? min(num, (long long)INT_MAX)
-                         : max(-num, (long long)INT_MIN);
+        while (i < s.size() && isdigit(s[i])) {
+            result = result * 10 + (s[i++] - '0');
+
+            if (sign == 1 && result > INT_MAX) return INT_MAX;
+            if (sign == -1 && -result < INT_MIN) return INT_MIN;
+        }
+
+        return sign * result;
     }
 };
