220f733a1c72d1a9365e0145580f37eb583c9972
Asmi Srivastava <srivasmi20@gmail.com>
2026-07-03 12:21:25 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (60.10%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> result;
-    void findsubsets(int i,vector<int>& nums,vector<int>& temp){
-        if(i>=nums.size()){ result.push_back(temp);
-        return;}
+    void pickornotpick(int i, vector<int> & nums, vector<int>& temp){
+        if(i>=nums.size()){
+         result.push_back(temp);
+         return;
+        }
         temp.push_back(nums[i]);
-        findsubsets(i+1,nums,temp);
+        pickornotpick(i+1,nums,temp);
         temp.pop_back();
-        findsubsets(i+1,nums,temp);
+        pickornotpick(i+1,nums,temp);
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int> temp;
-        findsubsets(0,nums,temp);
-        return result;
+     vector<int> temp;
+     pickornotpick(0,nums,temp);
+     return result;
     }
 };
