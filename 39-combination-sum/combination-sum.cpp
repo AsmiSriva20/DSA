5953a918afdbe3758ee3bfb773023dcd7390d176
Asmi Srivastava <srivasmi20@gmail.com>
2026-07-04 11:38:25 +0530

    Time: 3 ms (52.86%) | Memory: 13.9 MB (99.69%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     
-    vector<vector<int>> generate(int i,vector<int>& candidates,int target,vector<int> &stored,vector<vector<int>> &ans){
+    void generate(int i,vector<int>& candidates,int target,vector<int> &stored,vector<vector<int>> &ans){
         if(i==candidates.size()){
             if(target==0)  ans.push_back(stored);
-            return ans;
+            return;
         }
         if(candidates[i]<=target){
         stored.push_back(candidates[i]);
@@ -12,7 +12,8 @@ public:
         stored.pop_back();
         }
         generate(i+1,candidates,target,stored,ans);
-        return ans;
+        return;
+
 
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
