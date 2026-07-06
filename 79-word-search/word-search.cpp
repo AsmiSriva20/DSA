2bb0a4487fac0d532632ae64e24b3d99e9d568eb
Asmi Srivastava <srivasmi20@gmail.com>
2026-07-06 17:02:59 +0530

    Time: 238 ms (58.25%) | Memory: 10.4 MB (93.17%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int k) {
-        if (k == word.size()) return true;
-
-        if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size() ||
-            board[i][j] != word[k])
+    vector<pair<int,int>> directions= {{-1,0},{1,0},{0,-1},{0,1}};
+     
+    bool dfs(vector<vector<char>>& board, string &word,int i,int j,int idx){
+        if(idx==word.size()){
+            return true;
+        }
+        if(i<0||j<0||i>=board.size()||j>=board[0].size()||board[i][j]!=word[idx]){
             return false;
+        }
+        char temp= board[i][j];
+        board[i][j] = '%';
+        for(auto [x,y]: directions){
+            if(dfs(board,word,i+x,j+y,idx+1)) return true;
+        }
+        board[i][j]=temp;
+        return false;
 
-        char temp = board[i][j];
-        board[i][j] = '#';
-
-        bool found = dfs(board, word, i + 1, j, k + 1) ||
-                     dfs(board, word, i - 1, j, k + 1) ||
-                     dfs(board, word, i, j + 1, k + 1) ||
-                     dfs(board, word, i, j - 1, k + 1);
-
-        board[i][j] = temp;
-        return found;
+        
     }
 
+
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size(), n = board[0].size();
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (dfs(board, word, i, j, 0))
-                    return true;
-            }
+        int m=board.size();
+        int n=board[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if (dfs(board,word,i,j,0)) return true;
+            } 
         }
-
         return false;
     }
 };
