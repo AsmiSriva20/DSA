21a38c27853e34f01c2e5eeaec8c4adf76a15998
Asmi Srivastava <srivasmi20@gmail.com>
2026-08-06 18:59:33 +0530

    Time: 0 ms (100.00%) | Memory: 20 MB (43.56%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-   
-    void dfs(vector<vector<int>>& isConnected, vector<int>&vis, int node){
-         int n= isConnected.size();
-        vis[node]=1;
-        for(int i=0;i<n;i++){
-            int x= isConnected[node][i];
-            if(x==1 && !vis[i]){
-                dfs(isConnected,vis,i);
+    void bfs(int start, vector<vector<int>>& isConnected, vector<int>& vis) {
+        queue<int> q;
+        q.push(start);
+        vis[start] = 1;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (int i = 0; i < isConnected.size(); i++) {
+                if (isConnected[node][i] == 1 && !vis[i]) {
+                    vis[i] = 1;
+                    q.push(i);
+                }
             }
         }
-
     }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-         int n= isConnected.size();
-        vector<int>vis(n,0);
-        int num=0;
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                num++;
-                dfs(isConnected,vis,i);
-            }
 
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int> vis(n, 0);
+        int provinces = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                provinces++;
+                bfs(i, isConnected, vis);
+            }
         }
-        return num;
-        
+
+        return provinces;
     }
 };
