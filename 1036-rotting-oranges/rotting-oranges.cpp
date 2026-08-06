47647c84a96fe856a8bd2f4cfebd65a45d791fec
Asmi Srivastava <srivasmi20@gmail.com>
2026-08-07 01:08:42 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (70.33%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<int,int>>q;
-        int rows= grid.size();
+        int rows=grid.size();
         int cols=grid[0].size();
+        queue<pair<int,int>>q; 
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
                 if(grid[i][j]==2){
@@ -12,28 +12,27 @@ public:
             }
         }
 
-        vector<pair<int,int>> dir = {{-1,0},{1,0},{0,-1},{0,1}};
-
         int minutes=-1;
+        vector<pair<int,int>> dir={{-1,0},{1,0},{0,-1},{0,1}};
 
-        while(!q.empty()){
-            int size=q.size();
+          while(!q.empty()){
+            int n=q.size();
             minutes++;
-            while(size--){
-                auto curr=q.front();
-                q.pop();
-                int row=curr.first;
-                int col=curr.second;
-                for(auto it: dir){
-                    int nr=row+it.first;
-                    int nc=col+it.second;
-
-                    if( nr<rows && nc<cols && nr>=0 && nc>=0&&grid[nr][nc]==1){
-                        grid[nr][nc]=2;
-                        q.push({nr,nc});
-                    }
-                }
-
+            while(n--){
+              auto curr=q.front();
+              q.pop();
+              int row=curr.first;
+              int col=curr.second;
+             for(auto it: dir){
+                int nr=row+it.first;
+                int nc=col+it.second;
+             
+             if(nr>=0 && nr<rows && nc>=0 && nc<cols && grid[nr][nc]==1){
+                grid[nr][nc]=2;
+                q.push({nr,nc});
+             }
+              
+            }  
             }
         }
         for(int i=0;i<rows;i++){
@@ -41,7 +40,7 @@ public:
                 if(grid[i][j]==1) return -1;
             }
         }
-        return (minutes==-1)? 0: minutes;
+        return (minutes==-1)? 0:minutes;
         
     }
 };
