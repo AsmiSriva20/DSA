a3c5fbb2b277ae13b7623eecf9d704016663e98d
Asmi Srivastava <srivasmi20@gmail.com>
2026-08-07 00:33:27 +0530

    Time: 0 ms (100.00%) | Memory: 16.9 MB (51.65%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,78 +1,47 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue <pair <int, int>> q;
-
-        int m = grid.size();
-        int n = grid[0].size();
-
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                if(grid[i][j] == 2){
-                    q.push({i , j});
+        queue<pair<int,int>>q;
+        int rows= grid.size();
+        int cols=grid[0].size();
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
                 }
             }
         }
 
-        int ans = -1;
+        vector<pair<int,int>> dir = {{-1,0},{1,0},{0,-1},{0,1}};
+
+        int minutes=-1;
 
         while(!q.empty()){
-            int sz = q.size();
-            ans++;
-
-            for(int i = 0; i<sz; i++){
-                auto f = q.front();
+            int size=q.size();
+            minutes++;
+            while(size--){
+                auto curr=q.front();
                 q.pop();
-                
-                int row = f.first;
-                int col = f.second;
+                int row=curr.first;
+                int col=curr.second;
+                for(auto it: dir){
+                    int nr=row+it.first;
+                    int nc=col+it.second;
 
-                if(row - 1 >= 0) {
-                    if(grid[row-1][col] == 1){
-                        grid[row-1][col] = 2;
-                        q.push({row-1 , col});
+                    if( nr<rows && nc<cols && nr>=0 && nc>=0&&grid[nr][nc]==1){
+                        grid[nr][nc]=2;
+                        q.push({nr,nc});
                     }
                 }
 
-                if(row + 1 < m){
-                    if(grid[row + 1][col] == 1){
-                        grid[row+1][col] = 2;
-                        q.push({row+1 , col});
-                    }
-                }
-
-
-                if(col - 1 >= 0){
-                    if(grid[row][col - 1] == 1){
-                        grid[row][col-1] = 2;
-                        q.push({row , col-1});
-                    }
-                }
-
-
-                if(col + 1 < n){
-                    if(grid[row][col + 1] == 1){
-                        grid[row][col+1] = 2;
-                        q.push({row , col+1});
-                    }
-                }
-            }
-
-            
-        }
-
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                if(grid[i][j] == 1){
-                    return -1;
-                }
             }
         }
-
-        if(ans == -1){
-            return 0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]==1) return -1;
+            }
         }
-
-        return ans;
+        return (minutes==-1)? 0: minutes;
+        
     }
 };
