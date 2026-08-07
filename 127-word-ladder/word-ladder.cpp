1bbeb4e5c4d952d580ae885707f7626449d5db95
Asmi Srivastava <srivasmi20@gmail.com>
2026-08-08 03:13:31 +0530

    Time: 750 ms (12.83%) | Memory: 33.5 MB (13.01%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> givenwords (wordList.begin(),wordList.end());
-        if(givenwords.find(endWord)==givenwords.end()) return 0;
-        queue<string>q;
+        unordered_set<string> words( wordList.begin(),wordList.end());
+        if(words.find(endWord)==words.end()) return 0;
+        queue<string> q;
         q.push(beginWord);
-        givenwords.erase(beginWord);
-
+        words.erase(beginWord);
         int steps=1;
 
         while(!q.empty()){
             int n=q.size();
             while(n--){
-                string word=q.front();
-                q.pop();
-                if(word==endWord) return steps;
-                for(int i=0;i<word.size();i++){
-                    char original=word[i];
-                    for(char ch='a';ch<='z';ch++){
-                        word[i]=ch;
-                        if(givenwords.find(word)!=givenwords.end()){
-                            q.push(word);
-                            givenwords.erase(word);
-                        }
+               string word=q.front();
+               q.pop();
+                   if (word == endWord)
+                    return steps;
+               words.erase(word);
+               for(int i=0;i<word.size();i++){
+                  char original=word[i];
+                  for(char ch='a';ch<='z';ch++){
+                    word[i]=ch;
+                    if(words.find(word)!=words.end()){
+                        q.push(word);
+                        
                     }
-                    word[i]=original;
-                }
+                  }
+                  word[i]=original;
+               }
+               
+               
             }
             steps++;
-            
         }
-     return 0;
-
-        
+        return 0;
     }
 };
