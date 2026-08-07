cbce601e0e48713a5a16e5d1dfda27e889bab106
Asmi Srivastava <srivasmi20@gmail.com>
2026-08-08 03:14:12 +0530

    Time: 51 ms (75.86%) | Memory: 19.9 MB (88.01%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,37 +1,54 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> words( wordList.begin(),wordList.end());
-        if(words.find(endWord)==words.end()) return 0;
+    int ladderLength(string beginWord, string endWord,
+                     vector<string>& wordList) {
+
+        unordered_set<string> givenWords(wordList.begin(), wordList.end());
+
+        if (givenWords.find(endWord) == givenWords.end())
+            return 0;
+
         queue<string> q;
         q.push(beginWord);
-        words.erase(beginWord);
-        int steps=1;
 
-        while(!q.empty()){
-            int n=q.size();
-            while(n--){
-               string word=q.front();
-               q.pop();
-                   if (word == endWord)
+        givenWords.erase(beginWord);
+
+        int steps = 1;
+
+        while (!q.empty()) {
+
+            int n = q.size();
+
+            while (n--) {
+
+                string word = q.front();
+                q.pop();
+
+                if (word == endWord)
                     return steps;
-               words.erase(word);
-               for(int i=0;i<word.size();i++){
-                  char original=word[i];
-                  for(char ch='a';ch<='z';ch++){
-                    word[i]=ch;
-                    if(words.find(word)!=words.end()){
-                        q.push(word);
-                        
+
+                for (int i = 0; i < word.size(); i++) {
+
+                    char original = word[i];
+
+                    for (char ch = 'a'; ch <= 'z'; ch++) {
+
+                        word[i] = ch;
+
+                        if (givenWords.find(word) != givenWords.end()) {
+
+                            q.push(word);
+                            givenWords.erase(word);
+                        }
                     }
-                  }
-                  word[i]=original;
-               }
-               
-               
+
+                    word[i] = original;
+                }
             }
+
             steps++;
         }
+
         return 0;
     }
 };
