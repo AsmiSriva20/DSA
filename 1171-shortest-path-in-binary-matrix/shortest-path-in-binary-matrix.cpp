bae40774dc728bb2432316d73d4553bfbbe04168
Asmi Srivastava <srivasmi20@gmail.com>
2026-08-14 03:19:56 +0530

    Time: 28 ms (29.21%) | Memory: 23.7 MB (77.91%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n= grid.size();
-        if(grid[0][0]==1 || grid[n-1][n-1]==1) return -1;
-        if(n==1) return 1;
-        queue<pair<int,int>>q;
-        q.push({0,0});
-        grid[0][0]=1;
+        int n=grid.size();
+        if(grid[0][0]==1  || grid[n-1][n-1]==1) return -1;
+        using P= pair<int, pair<int,int>>;
+        priority_queue<P, vector<P>, greater<P>>pq;
+        vector<vector<int>>distance(n,vector<int>(n,INT_MAX));
 
-        vector<pair<int,int>> dir ={{1,0},{0,1},{1,1},{-1,-1},{-1,0},{0,-1},{-1,1},{1,-1}};
-        int steps=1;
-
-        while(!q.empty()){
-            int s=q.size();
-            while(s--){
-                auto curr = q.front();
-                q.pop();
-                int row= curr.first;
-                int col=curr.second;
-                for(auto it: dir){
-                    int nr=row+it.first;
-                    int nc=col+it.second;
-                    if(nr>=0 && nc>=0 && nr<n && nc<n && grid[nr][nc]==0){
-                        if(nr==n-1 && nc==n-1) return steps+1;
-                       q.push({nr,nc});
-                       grid[nr][nc]=1;
+        vector<pair<int,int>> dir = {{1,0}, {0,1}, {1,1}, {-1,-1},{-1,0}, {0,-1}, {-1,1}, {1,-1}};
+        pq.push({1, {0,0}});
+        distance[0][0] = 1;
+        while(!pq.empty()){
+            auto curr=pq.top();
+            pq.pop();
+            int dist=curr.first;
+            int row=curr.second.first;
+            int col=curr.second.second;
+            if(row==n-1 && col==n-1) return dist;
+            for(auto it: dir){
+                int nr= row+it.first;
+                int nc=col+it.second;
+                if(nr>=0 && nc>=0 && nr<n && nc<n && grid[nr][nc]==0){
+                    int nayadist=1+dist;
+                    if(nayadist<distance[nr][nc]){
+                        distance[nr][nc]=nayadist;
+                        pq.push({nayadist,{nr,nc}});
                     }
                 }
             }
-            steps++;
+
+
         }
-        return -1;
+
+   return -1;
+        
     }
 };
