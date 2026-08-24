27aefa571620e16780ea8a14505627dc6c7d27ce
Asmi Srivastava <srivasmi20@gmail.com>
2026-08-24 17:16:05 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (37.50%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-
-        for(char c : s) {
-            if(c == '(' || c == '{' || c == '[') {
-                st.push(c);
-            }
+        stack<char>st;
+        for(char ch : s){
+            if(ch=='(' || ch=='{' || ch=='[') st.push(ch);
             else {
                 if(st.empty()) return false;
-
-                if(c == ')' && st.top() != '(') return false;
-                if(c == '}' && st.top() != '{') return false;
-                if(c == ']' && st.top() != '[') return false;
-
+                if(ch==')' && st.top()!='(') return false;
+                if(ch=='}' && st.top()!='{') return false;
+                if(ch==']' && st.top()!='[') return false;
                 st.pop();
             }
+            
+            
+            
         }
-
         return st.empty();
     }
 };
