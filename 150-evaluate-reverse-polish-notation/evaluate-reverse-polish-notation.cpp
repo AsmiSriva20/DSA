e79a2893ec210e129f1ba347b4430ec42216be27
Asmi Srivastava <srivasmi20@gmail.com>
2026-08-29 00:09:20 +0530

    Time: 1 ms (49.07%) | Memory: 17.4 MB (9.63%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    int operate(int a,int b, string &ch){
-        if(ch=="+"){
-            return a+b;
-        }
-        if(ch=="-"){
-            return a-b;
-        }
-        if(ch=="/"){
-            return a/b;
-        }
-        if(ch=="*"){
-            return a*b;
-        }
-        else return 0;
-    }
 
 
     int evalRPN(vector<string>& tokens) {
         stack<int>st;
         int result;
+        unordered_map<string, function<int (int, int) > > mpp = {
+            { "+" , [] (int a, int b) { return a + b; } },
+            { "-" , [] (int a, int b) { return a - b; } },
+            { "*" , [] (int a, int b) { return a * b; } },
+            { "/" , [] (int a, int b) { return a / b; } }
+        };
+
         for(int i=0;i<tokens.size();i++){
             if(tokens[i]=="+"||tokens[i]=="-"||tokens[i]=="/"||tokens[i]=="*"){
                 int b=st.top(); st.pop();
                 int a=st.top();st.pop();
-                result=operate(a,b,tokens[i]);
+                result=mpp[tokens[i]](a,b);
                 st.push(result);
             }
             else st.push(stoi(tokens[i]));
